c353a3aad4b5f8c7aa90ebf52a7b17794e3a407d
Jontas77 <mrjbheugh@gmail.com>
2021-08-03 22:32:09 +0200

    Completed Q 1 - 3

--- jontaConnect2.session.sql
+++ jontaConnect2.session.sql
@@ -1,3 +1,13 @@
 SELECT * FROM expense_areas;
 
 SELECT * FROM expense_types;
+
+SELECT * FROM spends;
+
+SELECT * FROM suppliers;
+
+SELECT date, transaction_no, description FROM spends WHERE amount BETWEEN 30000 AND 31000;
+
+SELECT date, transaction_no, supplier_inv_no, description, amount FROM spends WHERE description LIKE '%fee%';
+
+SELECT date, transaction_no, supplier_inv_no, description, amount FROM spends WHERE description LIKE '%Fee%';
